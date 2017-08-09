d4fc1926e34d0b2e8b9c73ac7c27b9606cab1164
Taillook <jousoku@gmail.com>
2017-08-09 15:22:13 +0900

    [Add] Narou4s project

---
+++ Narou4s/Narou4s.h
@@ -0,0 +1,19 @@
+//
+//  Narou4s.h
+//  Narou4s
+//
+//  Created by Taillook on 2017/08/09.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Narou4s.
+FOUNDATION_EXPORT double Narou4sVersionNumber;
+
+//! Project version string for Narou4s.
+FOUNDATION_EXPORT const unsigned char Narou4sVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Narou4s/PublicHeader.h>
+
+
